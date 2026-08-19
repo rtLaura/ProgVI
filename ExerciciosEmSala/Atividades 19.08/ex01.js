5e5a2554eb8cbd7f8c6d206bbcba3911713c5aa7
Laura <laurarobettegrazziolli@gmail.com>
2026-08-19 16:25:22 -0300

    .

--- ExerciciosEmSala/Atividades 19.08/ex01.js
+++ ExerciciosEmSala/Atividades 19.08/ex01.js
@@ -1,1 +1,8 @@
 /*Percorra o vetor numeros = [2, 4, 6, 8, 10] com a função MAP*/
+
+var numeros = [2, 4, 6, 8, 10];
+var divisao = numeros.map((elem) => elem / 2);
+
+for (var i = 0; i < numeros.lenght; i++) {
+    console.log(divisao[i])
+}
