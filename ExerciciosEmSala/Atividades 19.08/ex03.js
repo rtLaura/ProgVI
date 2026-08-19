3448a21236115e52c2e9511a395c0f369657fe6e
Laura <laurarobettegrazziolli@gmail.com>
2026-08-19 16:44:23 -0300

    Update ex03.js

--- ExerciciosEmSala/Atividades 19.08/ex03.js
+++ ExerciciosEmSala/Atividades 19.08/ex03.js
@@ -1,1 +1,6 @@
 /*refaça o exercicio 1, agora use o map para fazer o triplo dos valores e em seguida use o próprio map para imprimir o novo vetor*/
+var numeros = [2, 4, 6, 8, 10];
+
+var mult = numeros.map((elem) => elem * 3);
+
+mult.map((elem) => console.log(elem));
